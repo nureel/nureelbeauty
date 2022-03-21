3f16a92835a6616f4f7c2ec626441007de79e0ef
nureel <support@nureelbeauty.com>
2022-03-21 16:51:21 +0800

    sentry

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,30 @@
 import * as cookie from 'cookie';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
+
+/** @type {import('@sveltejs/kit').HandleError} */
+export async function handleError({ error, event }) {
+  Sentry.init({
+		dsn: "https://f27e5acb8fb04503a8de8bcd18162a2e@o1173486.ingest.sentry.io/6268593",
+		tracesSampleRate: 1.0,
+	});
+	
+	const transaction = Sentry.startTransaction({
+		op: "test",
+		name: "My First Test Transaction",
+	});
+	
+	// setTimeout(() => {
+	// 	try {
+	// 		foo();
+	// 	} catch (e) {
+	// 		Sentry.captureException(e);
+	// 	} finally {
+	// 		transaction.finish();
+	// 	}
+	// }, 99);
+  Sentry.captureException(error, { event });
+}
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
