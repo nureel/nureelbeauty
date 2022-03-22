41d05164a8253c9795088a95d5110124249aaf37
nureel <support@nureelbeauty.com>
2022-03-22 12:53:07 +0800

    email

--- src/routes/api/email.js
+++ src/routes/api/email.js
@@ -2,8 +2,6 @@ import SibApiV3Sdk from 'sib-api-v3-sdk';
 
 export async function post({ request }) {
     const data = await request.json();
-     
-    let status;
     let defaultClient = SibApiV3Sdk.ApiClient.instance;
     let apiKey = defaultClient.authentications['api-key'];
     apiKey.apiKey = import.meta.env.VITE_SIB_KEY;
@@ -12,7 +10,7 @@ export async function post({ request }) {
 
     sendSmtpEmail.subject = "Feedback";
     sendSmtpEmail.htmlContent = "<html><body><h1>{{params.parameter}}</h1></body></html>";
-    sendSmtpEmail.sender = {"name":data.name,"email":""};
+    sendSmtpEmail.sender = {"name":data.name,"email":data.email};
     sendSmtpEmail.to = [{"email":"cs@nureelbeauty.com","name":"Nureel Beauty"}];
     sendSmtpEmail.headers = {"Some-Custom-Name":"unique-id-1234"};
     sendSmtpEmail.params = {"parameter":data.feedback,"subject":"New Subject"};
@@ -20,7 +18,7 @@ export async function post({ request }) {
     apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
       console.log('API called successfully. Returned data: ' + JSON.stringify(data));
     }, function(error) {
-      console.error(error);
+      // console.error(error);
     });
 
     return {
