68288384d66b7ecf5d845c3023ff08d149366e4e
nureel <support@nureelbeauty.com>
2022-03-22 23:34:49 +0800

    update

--- src/routes/api/signin.js
+++ src/routes/api/signin.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import bcryptjs from 'bcryptjs';
 import Account from '$lib/models/account';
 const uri = import.meta.env.VITE_DB_URI;
-mongoose.connect(uri);
+// mongoose.connect(uri);
 
 export async function post({ request }) {
 	const data = await request.json();
